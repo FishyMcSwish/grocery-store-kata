9becc5bbb69b381abad05269d062b62ee97f150e
Grant <grant.maki@integral.io>
2021-04-24 15:14:43 -0400

    add vararg constructor far basket

--- app/src/main/java/grocery/store/Basket.java
+++ app/src/main/java/grocery/store/Basket.java
@@ -1,12 +1,18 @@
 package grocery.store;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Basket {
     private ArrayList<Item> items;
 
     public Basket() {
         items = new ArrayList<>();
+    }
+    
+    public Basket(Item... items){
+        this.items = new ArrayList();
+        Collections.addAll(this.items, items); 
     }
 
     public ArrayList<Item> add(Item item) {
--- app/src/test/java/grocery/store/BasketTest.java
+++ app/src/test/java/grocery/store/BasketTest.java
@@ -7,45 +7,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BasketTest {
 
     @Test
-    public void basketsCanAddItems(){
+    public void basketsCanAddItems() {
         Basket basket = new Basket();
         basket.add(Item.SOUP);
         basket.add(Item.APPLE);
         assertThat(basket.getItems()).containsExactly(Item.SOUP, Item.APPLE);
     }
+
     @Test
     public void basketCanPrice_OneSoup() {
-        Basket basket = new Basket();
-        basket.add(Item.SOUP);
+        Basket basket = new Basket(Item.SOUP);
         assertThat(basket.price()).isEqualTo(65);
     }
+
     @Test
     public void basketCanPrice_OneApple() {
-        Basket basket = new Basket();
-        basket.add(Item.APPLE);
+        Basket basket = new Basket(Item.APPLE);
         assertThat(basket.price()).isEqualTo(10);
     }
+
     @Test
     public void basketCanPrice_OneBread() {
-        Basket basket = new Basket();
-        basket.add(Item.BREAD);
+        Basket basket = new Basket(Item.BREAD);
         assertThat(basket.price()).isEqualTo(80);
     }
 
     @Test
-    public void basketCanPrice_OneMilk(){
-        Basket basket = new Basket();
-        basket.add(Item.MILK);
+    public void basketCanPrice_OneMilk() {
+        Basket basket = new Basket(Item.MILK);
         assertThat(basket.price()).isEqualTo(130);
     }
-    
+
     @Test
     public void basketCanPrice_OneOfEach() {
-        Basket basket = new Basket();
-        basket.add(Item.MILK);
-        basket.add(Item.APPLE);
-        basket.add(Item.SOUP);
-        basket.add(Item.BREAD);
+        Basket basket = new Basket(Item.MILK, Item.APPLE, Item.SOUP, Item.BREAD);
 
         assertThat(basket.price()).isEqualTo(285);
     }
