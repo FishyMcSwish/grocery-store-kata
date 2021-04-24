bb8c7d3d1b5e712c17adbd8e3df5de5d75fdfd79
Grant <grant.maki@integral.io>
2021-04-24 15:25:17 -0400

    add test for return value of Basket.add()

--- app/src/test/java/grocery/store/BasketTest.java
+++ app/src/test/java/grocery/store/BasketTest.java
@@ -2,6 +2,8 @@ package grocery.store;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BasketTest {
@@ -11,6 +13,15 @@ public class BasketTest {
         Basket basket = new Basket();
         basket.add(Item.SOUP);
         basket.add(Item.APPLE);
+        
         assertThat(basket.getItems()).containsExactly(Item.SOUP, Item.APPLE);
+    }
+
+    @Test
+    public void addingItems_ReturnsAllItemsInBasket() {
+        Basket basket = new Basket(Item.APPLE);
+        ArrayList<Item> itemsReturned = basket.add(Item.SOUP);
+        
+        assertThat(itemsReturned).isEqualTo(basket.getItems());
     }
 }
