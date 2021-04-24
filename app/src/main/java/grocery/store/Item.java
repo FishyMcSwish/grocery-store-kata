f27e06526a6377e18b22da6bb2407c3c0e5b1989
Grant <grant.maki@integral.io>
2021-04-24 14:57:12 -0400

    rename Item.APPLES to Item.APPLE -- its only one apple

--- app/src/main/java/grocery/store/Item.java
+++ app/src/main/java/grocery/store/Item.java
@@ -1,7 +1,7 @@
 package grocery.store;
 
 public enum Item{
-    SOUP(65), BREAD(80), MILK(130), APPLES(10);
+    SOUP(65), BREAD(80), MILK(130), APPLE(10);
     private int cents;
     private Item(int cents){
         this.cents = cents;
--- app/src/test/java/grocery/store/BasketTest.java
+++ app/src/test/java/grocery/store/BasketTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BasketTest {
+    
     @Test
     public void basketsCanAddItems(){
         Basket basket = new Basket();
         basket.add(Item.SOUP);
-        basket.add(Item.APPLES);
-        assertThat(basket.getItems()).containsExactly(Item.SOUP, Item.APPLES);
+        basket.add(Item.APPLE);
+        assertThat(basket.getItems()).containsExactly(Item.SOUP, Item.APPLE);
     }
     
 }
